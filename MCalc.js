c66334c79825ea3a80e2332ca0cb77c9afb358d3
maqnaveed <153424211+maqnaveed@users.noreply.github.com>
2023-12-28 17:55:49 +0500

    Add files via upload

---
+++ MCalc.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+async function startCalu() {
+    console.log(`Mini Calculatir \n`);
+    const userInput = await inquirer.prompt([
+        {
+            type: "number",
+            name: "num1",
+            message: "Enter your First Number"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter your Second Number"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["Add", "Sub", "Multiply", "Divide"]
+        }
+    ]);
+    const userOp = userInput.operator;
+    const firstNum = userInput.num1;
+    const secondNum = userInput.num2;
+    if (userOp == "Add") {
+        console.log(`\n The Answer of ${firstNum} + {secondNum} = ${firstNum + secondNum}`);
+    }
+    else if (userOp == "Sub") {
+        console.log(`\n The Answer of ${firstNum} - {secondNum} = ${firstNum - secondNum}`);
+    }
+    else if (userOp == "Multiply") {
+        console.log(`\n The Answer of ${firstNum} * {secondNum} = ${firstNum * secondNum}`);
+    }
+    else if (userOp == "Divide") {
+        console.log(`\n The Answer of ${firstNum} / {secondNum} = ${firstNum / secondNum}`);
+    }
+}
+startCalu();
